589015fced29b4e8ad6ca256af4280f7fdb390fc
Carrizo Avellaneda Agustin <carrizoaagustin@gmail.com>
2024-10-25 20:37:55 -0300

    add test utils

---
+++ test_utils/init.go
@@ -0,0 +1,34 @@
+package testutils
+
+import (
+	"github.com/carrizoaagustin/cv-online/config"
+	"github.com/carrizoaagustin/cv-online/pkg/dbconnection"
+	"github.com/carrizoaagustin/cv-online/pkg/dbquerybuilder"
+)
+
+type TestDBResources struct {
+	DBquerybuilder *dbquerybuilder.DBQueryBuilder
+	DBconnection   *dbconnection.DatabaseConnection
+}
+
+func InitTestDBResources() *TestDBResources {
+	cfg := config.LoadTestConfig()
+
+	databaseConnection := dbconnection.New(&cfg.DatabaseConfig)
+	databaseConnection.CreateSchema()
+	databaseConnection.Connect()
+
+	databaseConnection.RunMigrations()
+
+	// INIT OBJECTS
+	queryBuilder := dbquerybuilder.New(databaseConnection.GetDatabaseConnection())
+
+	return &TestDBResources{
+		DBquerybuilder: queryBuilder,
+		DBconnection:   databaseConnection,
+	}
+}
+
+func CloseTestDBResources(resource *TestDBResources) {
+	resource.DBconnection.Close()
+}
