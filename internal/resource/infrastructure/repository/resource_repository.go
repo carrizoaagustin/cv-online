e92b20a5737f9b5f8df5b16f9e4db610d827fa21
Carrizo Avellaneda Agustin <carrizoaagustin@gmail.com>
2024-10-25 20:41:19 -0300

    reformat code

--- internal/resource/infrastructure/repository/resource_repository.go
+++ internal/resource/infrastructure/repository/resource_repository.go
@@ -17,9 +17,14 @@ func NewResourceRepository(queryBuilder *dbquerybuilder.DBQueryBuilder) *Resourc
 
 func (r *ResourceRepository) Create(resource domain.Resource) error {
 	// USE EXAMPLE FOR MIGUEL
-	_, err := r.queryBuilder.StartQuery().Insert("resources").Rows(
-		dbquerybuilder.Record{"resource_id": resource.ID, "format": resource.Format},
-	).Executor().Exec()
+	_, err := r.queryBuilder.
+		StartQuery().
+		Insert("resources").
+		Rows(
+			dbquerybuilder.Record{"resource_id": resource.ID, "format": resource.Format},
+		).
+		Executor().
+		Exec()
 
 	if err != nil {
 		return err
