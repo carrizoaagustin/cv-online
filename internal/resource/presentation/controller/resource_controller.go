3da093b19271ae17850266058265e8b81cd375df
Carrizo Avellaneda Agustin <carrizoaagustin@gmail.com>
2024-12-02 02:05:38 -0300

    wip

--- internal/resource/presentation/controller/resource_controller.go
+++ internal/resource/presentation/controller/resource_controller.go
@@ -1,4 +1,61 @@
 package controller
 
+import (
+	"bytes"
+	"github.com/carrizoaagustin/cv-online/internal/resource/application"
+	"github.com/carrizoaagustin/cv-online/internal/resource/application/dto"
+	"github.com/gin-gonic/gin"
+	"io"
+)
+
 type ResourceController struct {
+	resourceUseCase application.ResourceUseCase
+}
+
+func NewResourceController(resourceUseCase application.ResourceUseCase) *ResourceController {
+	return &ResourceController{
+		resourceUseCase: resourceUseCase,
+	}
+}
+
+func (c *ResourceController) UploadFile(ctx *gin.Context) {
+	var form struct {
+		Filename    string `form:"filename" binding:"required"`
+		ContentType string `form:"content_type" binding:"required"`
+	}
+
+	err := ctx.ShouldBind(&form)
+	if err != nil {
+		return
+	}
+
+	file, err := ctx.FormFile("file")
+	if err != nil {
+		return
+	}
+
+	src, err := file.Open()
+	if err != nil {
+		return
+	}
+
+	defer src.Close()
+
+	var buffer bytes.Buffer
+
+	if _, err := io.Copy(&buffer, src); err != nil {
+		return
+	}
+
+	input := dto.UploadResourceDTO{
+		File:        buffer.Bytes(),
+		Filename:    form.Filename,
+		ContentType: form.ContentType,
+	}
+
+	err = c.resourceUseCase.UploadResource(input)
+	if err != nil {
+		return
+	}
+
 }
